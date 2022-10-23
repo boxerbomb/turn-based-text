b31a9e7101679cb2bda108c2e9521ea4cbe09943
Nathan Henry <nathanhenry1999@gmail.com>
2022-10-23 06:16:18 -0400

    HEadless Update

--- pygame_ex1.py
+++ pygame_ex1.py
@@ -1,5 +1,8 @@
 import pygame
 from pygame.locals import *
+import os
+
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 BLOCK_SIZE=16
 SCREEN_WIDTH = (BLOCK_SIZE*20)+(1*BLOCK_SIZE)
@@ -11,7 +14,7 @@ BLUE = (0,0,255)
 GRAY = (150, 150, 150)
 
 pygame.init()
-screen = pygame.display.set_mode(SIZE)
+canvas = pygame.Surface(SIZE)
 
 board = [
 [0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
@@ -113,55 +116,50 @@ b_roof_edge = pygame.image.load("res/b_roof_edge.png")
 b_chiminey = pygame.image.load("res/b_chiminey.png")
 
 
+for event in pygame.event.get():
+    if event.type == QUIT:
+        running = False
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-    
-    screen.fill(GRAY)
+canvas.fill(GRAY)
 
-    screen.blit(grid_image,(0,0))
+canvas.blit(grid_image,(0,0))
 
-    # Backdrop
-    for y in range(0,20):
-        for x in range(0,20):
-            color_index = board[y][x]
-            if color_index==0:
-                grass_index = hash(hash(x)-hash(y)+hash(x*x)-hash(x*y))%18
-                screen.blit(grass_tiles[grass_index], ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
-            else:
-                floor_index = hash(hash(y)-hash(x)+hash(y*y-x)-hash(x*y))%7
-                screen.blit(floor_tiles[floor_index], ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+# Backdrop
+for y in range(0,20):
+    for x in range(0,20):
+        color_index = board[y][x]
+        if color_index==0:
+            grass_index = hash(hash(x)-hash(y)+hash(x*x)-hash(x*y))%18
+            canvas.blit(grass_tiles[grass_index], ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+        else:
+            floor_index = hash(hash(y)-hash(x)+hash(y*y-x)-hash(x*y))%7
+            canvas.blit(floor_tiles[floor_index], ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
 
-    # Objects
-    for y in range(0,20):
-        for x in range(0,20):
-            tile_index = obj_board[y][x]
-            if tile_index==0:
-                pass
-            elif tile_index==1:
-                tall_grass_index = hash(hash(y)-hash(x)+hash(y*y-x)-hash(x*y))%4
-                screen.blit(tall_grass_tiles[tall_grass_index], ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
-            elif tile_index==30:
-                screen.blit(b_wall, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
-            elif tile_index==31:
-                screen.blit(b_door, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
-            elif tile_index==32:
-                screen.blit(b_roof, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
-            elif tile_index==33:
-                screen.blit(b_roof_edge, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
-            elif tile_index==34:
-                screen.blit(b_chiminey, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+# Objects
+for y in range(0,20):
+    for x in range(0,20):
+        tile_index = obj_board[y][x]
+        if tile_index==0:
+            pass
+        elif tile_index==1:
+            tall_grass_index = hash(hash(y)-hash(x)+hash(y*y-x)-hash(x*y))%4
+            canvas.blit(tall_grass_tiles[tall_grass_index], ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+        elif tile_index==30:
+            canvas.blit(b_wall, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+        elif tile_index==31:
+            canvas.blit(b_door, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+        elif tile_index==32:
+            canvas.blit(b_roof, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+        elif tile_index==33:
+            canvas.blit(b_roof_edge, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
+        elif tile_index==34:
+            canvas.blit(b_chiminey, ((x+1)*BLOCK_SIZE,(y+1)*BLOCK_SIZE))
 
-    # Debug Lines
-    #for x in range(0,SCREEN_WIDTH,BLOCK_SIZE):
-    #    pygame.draw.line(screen, RED, (x,0), (x,SCREEN_HEIGHT))
-    #for y in range(0,SCREEN_HEIGHT,BLOCK_SIZE):
-    #    pygame.draw.line(screen, RED, (0,y), (SCREEN_WIDTH,y))
+# Debug Lines
+#for x in range(0,SCREEN_WIDTH,BLOCK_SIZE):
+#    pygame.draw.line(screen, RED, (x,0), (x,SCREEN_HEIGHT))
+#for y in range(0,SCREEN_HEIGHT,BLOCK_SIZE):
+#    pygame.draw.line(screen, RED, (0,y), (SCREEN_WIDTH,y))
 
-    pygame.display.flip()
-    #running=False
-pygame.image.save_extended(screen,"final.jpg")
+pygame.image.save_extended(canvas,"final.jpg")
 pygame.quit()
